25c808de2f377ab0ecce79c320cbcf9930876051
hyeyeon-sun <dlgpdus0416@naver.com>
2021-04-07 10:24:41 +0900

    Added detailed blog post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 '''동일한 디렉토리 내에 있기 때문에 .파일명을 붙이지 않아도 내용을 가져올 수 있다.'''
 
 # Create your views here.
@@ -8,3 +9,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
